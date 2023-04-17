0e2f7308760ff94c5b43c3d195299bba14c66299
“Ange-Constance” <constance.nimuhire@gmail.com>
2023-04-17 14:22:58 +0200

    update-user-profile

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ProfileUpdateForm from './features/profile/ProfileUpdateForm'
+import UserProfile from './features/profile/UserProfile'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    {/* <ProfileUpdateForm/> */}
+    <UserProfile/>
   </React.StrictMode>
 )
