b2944aed82b0d884b5ee9e91ddc76adb2106ef6b
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2023-04-26 10:07:33 +0200

    commt

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,15 @@ import React from 'react'
 import './assets/styles/index.css'
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './store'
 
 function App() {
-  const count = useSelector((state) => state.count)
-  const dispatch = useDispatch()
-
   return (
-    <>
-      <Router>
-        <Routes>
-          {/* auth routes */}
-          <Route path='/' element={<Home />} />
-        </Routes>
-      </Router>
-
-      <div>
-        <h1>Counter: {count}</h1>
-        <button onClick={() => dispatch(increment())}>Increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
-      </div>
-    </>
+    <Router>
+      <Routes>
+        {/* auth routes */}
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-function Home() {
+import Button from '../components/ui/Button'
+import Footer from '../components/footer/index'
 
+function Home() {
   return (
-    <div className="Home">
+    <div className='Home'>
       <h1>Welcome To Phantom</h1>
-      <div className="card">
-        Frontend
-      </div>
+      <div className='card'>Frontend</div>
+
+      <Footer />
     </div>
   )
 }
