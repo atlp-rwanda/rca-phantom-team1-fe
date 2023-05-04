338f40e3669aa2eda7c4a10da21cc2f876d978c9
SauveJeanLuc <sauvejeanluc3@gmail.com>
2023-05-04 17:16:04 +0200

    auth context

---
+++ src/context/auth.context.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+const initialState = {
+  user: null,
+};
+
+const AuthContext = createContext(initialState);
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+};
