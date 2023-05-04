aa84e54d0ecb44a36aea47fd48182bad3cf31fe0
SauveJeanLuc <sauvejeanluc3@gmail.com>
2023-05-04 17:15:05 +0200

    include DashBoard

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './assets/styles/index.css'
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import DashBoard from './pages/DashBoard'
 import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement } from './store'
 import PasswordReset from './pages/PasswordReset'
 import Login from './pages/Login' 
+import Dashboard from './pages/DashBoard'
 
 function App() {
   const count = useSelector((state) => state.count)
@@ -19,6 +21,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/password-reset' element={<PasswordReset />} />
           <Route path='/sign-in' element={<Login />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Routes>
       </Router>
     </>
