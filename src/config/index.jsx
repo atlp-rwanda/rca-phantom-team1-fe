43c4ca63b549d936f8248416e7f3bf879d0d5cc4
Cyebukayire <peacecyebukayire@gmail.com>
2023-05-08 14:10:47 +0200

    update config file

--- src/config/index.jsx
+++ src/config/index.jsx
@@ -1,5 +1,5 @@
-// const API_URL = "https://phantom-techbandit.onrender.com"
-const API_URL = "http://localhost:3000"
+const API_URL = "https://phantom-techbandit.onrender.com"
+// const API_URL = "http://localhost:3000"
 
 export {
     API_URL
